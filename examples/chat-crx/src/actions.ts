00ed46f04e3b9d042690e083fcb70c7881866138
Shivam Goyal <shivam.goyal@optexity.com>
2025-05-30 23:32:52 -0700

    multiple locator chaining

--- examples/chat-crx/src/actions.ts
+++ examples/chat-crx/src/actions.ts
@@ -5,49 +5,84 @@ export async function takeAction(action: any, page: Page) {
   if (action.next_action.locators == null) return false;
   if (action.next_action.locators.length == 0) return false;
 
+  let element: Locator | null = null;
   for (const locator of action.next_action.locators) {
-    let element: Locator;
     switch (locator.locator_type) {
       case "getByText":
-        element = page.getByText(locator.first_arg, locator.options);
+        if (element != null) {
+          element = element.getByText(locator.first_arg, locator.options);
+        } else {
+          element = page.getByText(locator.first_arg, locator.options);
+        }
         break;
       case "getByRole":
-        element = page.getByRole(locator.first_arg, locator.options);
+        if (element != null) {
+          element = element.getByRole(locator.first_arg, locator.options);
+        } else {
+          element = page.getByRole(locator.first_arg, locator.options);
+        }
         break;
       case "getByLabel":
-        element = page.getByLabel(locator.first_arg, locator.options);
+        if (element != null) {
+          element = element.getByLabel(locator.first_arg, locator.options);
+        } else {
+          element = page.getByLabel(locator.first_arg, locator.options);
+        }
         break;
       case "getByPlaceholder":
-        element = page.getByPlaceholder(locator.first_arg, locator.options);
+        if (element != null) {
+          element = element.getByPlaceholder(
+            locator.first_arg,
+            locator.options
+          );
+        } else {
+          element = page.getByPlaceholder(locator.first_arg, locator.options);
+        }
         break;
       case "getByAltText":
-        element = page.getByAltText(locator.first_arg, locator.options);
+        if (element != null) {
+          element = element.getByAltText(locator.first_arg, locator.options);
+        } else {
+          element = page.getByAltText(locator.first_arg, locator.options);
+        }
         break;
       case "getByTitle":
-        element = page.getByTitle(locator.first_arg, locator.options);
+        if (element != null) {
+          element = element.getByTitle(locator.first_arg, locator.options);
+        } else {
+          element = page.getByTitle(locator.first_arg, locator.options);
+        }
         break;
       case "getByTestId":
-        element = page.getByTestId(locator.first_arg);
+        if (element != null) {
+          element = element.getByTestId(locator.first_arg);
+        } else {
+          element = page.getByTestId(locator.first_arg);
+        }
         break;
       case "locator":
-        element = page.locator(locator.first_arg, locator.options);
+        if (element != null) {
+          element = element.locator(locator.first_arg, locator.options);
+        } else {
+          element = page.locator(locator.first_arg, locator.options);
+        }
         break;
       default:
         throw new Error(`Unknown locator type: ${locator.locator_type}`);
     }
-
-    switch (action.next_action_name) {
-      case "ClickElementAction":
-        if (action.next_action.double_click) {
-          await element.dblclick();
-        } else {
-          await element.click();
-        }
-        break;
-      case "InputTextAction":
-        await element.fill(action.next_action.text);
-        break;
-    }
+  }
+  if (element == null) return false;
+  switch (action.next_action_name) {
+    case "ClickElementAction":
+      if (action.next_action.double_click) {
+        await element.dblclick();
+      } else {
+        await element.click();
+      }
+      break;
+    case "InputTextAction":
+      await element.fill(action.next_action.text);
+      break;
   }
   return true;
 }
